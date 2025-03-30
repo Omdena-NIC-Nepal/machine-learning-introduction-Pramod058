8f89c2718f6061026b591f32f46bc5678a3b25fa
Pramod058 <aryalpramod05@gmail.com>
2025-03-30 16:42:23 +0545

    residuals under model-evaluation-done

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,1 +1,28 @@
-print("Hello world!!")
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
+
+def meanSquaredError(ytest, ypred):
+    return mean_squared_error(ytest, ypred)
+
+def meanAbsoluteError(ytest, ypred):
+    return mean_absolute_error(ytest, ypred)
+
+def r2Score(ytest, ypred):
+    return r2_score(ytest, ypred)
+
+
+def scatterplot(Y, ytest, ypred, alpha):
+    plt.scatter(ytest, ypred, alpha=0.5)
+    plt.plot([Y.min(), Y.max()], [Y.min(), Y.max()], color= "red")
+    plt.xlabel("Actual values")
+    plt.ylabel("Predicted values")
+    plt.title("Predicted vs Actual values")
+
+    return plt
+
+def residualplot(ypred, Hey, alpha):
+    plt.scatter(ypred, Hey, alpha=alpha)
+    plt.xlabel("Predicted values")
+    plt.ylabel("Residuals")
+    plt.title("Residual Plot (Linear Regression)")
+    return plt
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -9,3 +9,7 @@ def train_regression_model(xtrain, ytrain, whichModel):
     lin_model = whichModel.fit(xtrain, ytrain)
 
     return lin_model
+
+def y_pred(xtest, whichModel):
+    y_pred = whichModel.predict(xtest)
+    return y_pred
